a90636956a8e9074b1c0a0823a8fa54f5ff66d00
Devansh Jain <devansh@cutoz.com>
2018-02-22 23:21:54 +0530

    Update mongoConnect.js

--- app/Utilities/mongoConnect.js
+++ app/Utilities/mongoConnect.js
@@ -1,6 +1,1 @@
-var exports = module.exports = {};
-var mongoClient = require("mongodb").MongoClient;
-mongoClient.connect("mongodb://microsoftclub:pjqZG2MZNmexC6CJ4MTmPyIqDouhGSD2QR3xPDFQnBeSfFHwkwYrtiaUTIBcK0DJk3CljvGKYFLZ5aQsLblwhQ==@microsoftclub.documents.azure.com:10255/?ssl=true", 
-function (err, db) {
-  db.close();
-});
+
